8cbeece9bbeaee3551af15f29f5825c5ff5d9396
Gavin Rouleau <gjrcmb@yahoo.com>
2014-02-06 00:32:23 -0700

    Created User Registration & Login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,48 @@
 class UsersController < ApplicationController
 
   def login
+    if request.get?
+      # display login page
+    elsif request.post?
+      username = params[:username]
+      user = User.find_by user_name: "#{username}"
+      if !user.nil? && (user.authenticate? user, params)
+        flash[:notice] = "#{user.message}"
+        session[:user] = user
+        redirect_to "/users/welcome"
+      else
+        flash[:alert] = "Credentials Invalid. Please try again!"
+        session[:user] = nil
+      end
+    end
   end
 
-  def register
+  def registration
+  	if request.get?
+      # display registration page
+	  elsif request.post?
+
+      user = User.new(:user_name  => params[:new_username],
+                      :password   => params[:new_password],
+                      :user_email => params[:email_address])
+
+      if user.is_valid?(params)       
+        user.save
+        flash[:notice] = "#{user.message}"
+        redirect_to root_url
+      else
+        flash[:alert] = "#{user.message}"
+      end
+
+  	end	
   end
 
   def welcome
+    if !session[:user].nil?
+      # display welcome page
+    else
+      redirect_to "/users/login"
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,62 @@
 class User < ActiveRecord::Base
+
+  attr_accessor :message
+
+
+  def is_valid?(attributes)
+
+    new_username = attributes[:new_username]
+    new_password = attributes[:new_password]
+    new_password_confirm = attributes[:new_password_confirm]
+    email_address = attributes[:email_address]
+
+    if !is_username_valid? new_username
+      @message = "Username not valid. Please try again!"
+      nil
+    elsif !are_passwords_equal? new_password, new_password_confirm
+      @message = "Passwords entries must be identical. Please try again!"
+      nil
+    elsif !is_password_valid? new_password
+      @message = "Password entry is not valid. Please try again!"
+      nil      
+    elsif !is_email_valid? email_address
+      @message = "Email addess not valid. Please try again!"
+      nil
+    else
+      @message = "Registration Successful. Please Login!"
+      !nil      
+    end
+
+  end
+
+
+  def authenticate?(user, attributes)
+
+    if user[:password] == attributes[:password]
+      @message = "Login Successful."
+    end
+
+    user[:password] == attributes[:password]
+
+  end
+
+
+  private
+
+  def is_username_valid?(new_username)
+    !new_username.blank?
+  end
+
+  def are_passwords_equal?(new_password, new_password_confirm)
+    new_password.eql? new_password_confirm
+  end
+
+  def is_password_valid?(new_password)
+    !new_password.blank?
+  end
+
+  def is_email_valid?(email_address)
+    !email_address.blank?
+  end
+
 end
