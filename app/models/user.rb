a7bd07c26adb0248c94fa6fd45bc66dbfd9ed3f4
Gavin Rouleau <gjrcmb@yahoo.com>
2014-02-08 23:48:14 -0700

    Additional Validation Improvements

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,21 @@ class User < ActiveRecord::Base
 
   attr_accessor :message
 
-  validates :user_name, presence: 
-    { message: "Username not valid. Please try again!" }
-  validates :password, confirmation: 
-    { message: "Passwords entries must be identical. Please try again!" }
-  validates :password_confirmation, presence: 
-    { message: "Password entry is not valid. Please try again!" }
-  validates :user_email, presence: 
-    { message: "Email addess not valid. Please try again!" }
+  validates :user_name, 
+    presence: { message: "Username not valid. Please try again!" },
+    uniqueness: { message: "Username already taken. Please try another!" },
+    length: { maximum: 20, too_long: "Username too long.  
+                                      Maximum %{count} characters allowed!" }
+  validates :password, 
+    confirmation: { message: "Passwords entries must be identical. Please try again!" }
+  validates :password_confirmation, 
+    presence: { message: "Password entry is not valid. Please try again!" },
+    length: { maximum: 20, too_long: "Password too long.  
+                                      Maximum %{count} characters allowed!" }
+  validates :user_email, 
+    presence: { message: "Email addess not valid. Please try again!" },
+    length: { maximum: 40, too_long: "Email too long.  
+                                      Maximum %{count} characters allowed!" }
 
 
   def authenticate?(user, attributes)
